e6b440a13312f32597d8c8e26962036d8eda7056
Jacob Kastensmidt <jksmidt@gmail.com>
2026-04-17 16:04:26 -0400

    correct configuration to fix test issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,22 @@
 import { defineConfig } from "vite-plus";
-import { playwright } from "vite-plus/test/browser-playwright";
 import devtoolsJson from "vite-plugin-devtools-json";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-  lint: { options: { typeAware: true, typeCheck: true } },
+  lint: {
+    options: { typeAware: true, typeCheck: true },
+    ignorePatterns: ["context/**"],
+  },
+  fmt: {
+    ignorePatterns: ["context/**"],
+  },
   staged: {
     "*": "vp check --fix",
-  },
-  server: {
-    fs: {
-      allow: ["../../.."],
-    },
   },
   plugins: [sveltekit(), devtoolsJson()],
   test: {
     expect: { requireAssertions: true },
-    projects: [
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "client",
-          browser: {
-            enabled: true,
-            provider: playwright(),
-            instances: [{ browser: "chromium", headless: true }],
-          },
-          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-          exclude: ["src/lib/server/**"],
-        },
-      },
-
-      {
-        extends: "./vite.config.ts",
-        test: {
-          name: "server",
-          environment: "node",
-          include: ["src/**/*.{test,spec}.{js,ts}"],
-          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-        },
-      },
-    ],
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{js,ts}"],
   },
 });
